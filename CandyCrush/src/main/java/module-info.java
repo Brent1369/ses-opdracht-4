7560d2542269aae93e150448274641d62d07587c
Brent Vreys <vreysbrent@gmail.com>
2024-04-01 16:33:53 +0200

    part 8

--- CandyCrush/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module be.kuleuven.candycrush {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.controlsfx.controls;
-
-    opens be.kuleuven.candycrush to javafx.fxml;
-    exports be.kuleuven.candycrush;
-}
